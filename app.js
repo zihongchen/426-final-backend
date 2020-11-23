865f991f3b9d1ee094efd6f478809d6f9d3747c2
Zihongz <dsdvincent@gmail.com>
2020-11-23 01:32:58 -0500

    l

--- app.js
+++ app.js
@@ -60,4 +60,4 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(PORT, () => console.log(`Example app listening on port ${port}!`))
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
