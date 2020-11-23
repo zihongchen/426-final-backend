ab4699578ee5b1f9022dd12be3c5efa03a7bdee9
Zihongz <dsdvincent@gmail.com>
2020-11-22 22:16:05 -0500

    test.js

--- test.js
+++ test.js
@@ -1,9 +1,36 @@
-const fetch = require("node-fetch");
-let a = async function(){
-    const result = await fetch({
-        method: 'post',
+// const fetch = require("node-fetch");
+const axios = require('axios')
+
+// let a = async function(){
+//     const result = await fetch({
+//         method: 'get',
+//         //TODO: change add backend @zihongchen
+//         url: "http://localhost:3001/alumni",
+//         withCredentials: true,
+//         data: {
+//           "password" :"helloWorld",
+//           "email" : "Broscience@gmail.com"
+//         }
+//     }).then(res => {
+//         console.log(res.body)
+//     })
+//     .catch(error => console.log(error))
+// }
+
+// (async () => {
+
+    
+// })();
+let url = "url: 'http://localhost:3001/auth/login'"
+let test = async ()=>{
+    //const res = axios.get('http://localhost:3001/alumni');
+    
+    const result = await axios.get(url,
+        
+        {
+        method: 'get',
         //TODO: change add backend @zihongchen
-        url: "http://localhost:3001/auth/login",
+
         withCredentials: true,
         data: {
           "password" :"helloWorld",
@@ -13,6 +40,10 @@ let a = async function(){
         console.log(res.body)
     })
     .catch(error => console.log(error))
+
+
+    console.log("hello")
+    
 }
 
-a()
+test()
